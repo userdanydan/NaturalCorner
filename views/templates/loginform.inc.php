0d8dc4efd2df5b9f17aa74c5394b13328c02df8b
userdanydan <userdany.dan1952@laposte.net>
2016-01-10 16:36:46 +0100

    mise à jour du CSS.

--- views/templates/loginform.inc.php
+++ views/templates/loginform.inc.php
@@ -1,5 +1,5 @@
 <!-- cop col du site Bootstrap -->
-// <?php 
+<?php 
 // 	$fb = new Facebook\Facebook([
 // 			'app_id' => '479325815604386',
 // 			'app_secret' => '222768909de970fe4931805415d01b07',
@@ -12,10 +12,16 @@
 
 	
 // ?>
+</br>
+</br>
+</br>
+</br>
+</br>
+
 <div class="col-xs-1 col-md-4 col-lg-4"></div>
 <form role="form" class="col-xs-10 col-md-4 col-lg-4" method="post" action="index.php?action=Login">
   <div class="form-group">
-    <label for="nickname">Pseudo:</label>
+    <label for="nickname">Email:</label>
     <input type="text" class="form-control" name="nickname" id="nickname">
   </div>
   <div class="form-group">
